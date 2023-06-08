50cf29455697e57a6577e07c0181800766650b01
Fiama Brenda <fiamabrenda94@gmail.com>
2023-06-07 21:59:17 -0300

    Add files via upload
    Atividade de responsividade.

---
+++ DoaPetz-main - Copy/home/script.js
@@ -0,0 +1,14 @@
+let menu = document.getElementById("menu_links");
+let menu_barras = document.getElementById("menu_barras");
+
+function mostrarMenu (){
+    if(window.getComputedStyle(menu).display == "none"){
+        menu.style.display = "initial"
+        menu_barras.setAttribute("aria-label","fechar menu")
+        menu_barras.setAttribute("aria-expande","true")
+    }else{
+        menu.style.display = "none"
+        menu_barras.setAttribute("aria-label","abrir menu")
+        menu_barras.setAttribute("aria-expande","false")
+    }
+}
